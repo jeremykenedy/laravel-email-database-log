ef3eee209b46528f895cadf3d8854eba8e0dc9d0
Max Destors <max.destors@gmail.com>
2025-09-13 11:36:26 +0200

    add laravel 12 compatibility

--- tests/Feature/LaravelEmailDatabaseTest.php
+++ tests/Feature/LaravelEmailDatabaseTest.php
@@ -4,17 +4,19 @@ namespace jeremykenedy\LaravelEmailDatabaseLog\Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use jeremykenedy\LaravelEmailDatabaseLog\Tests\Mail\TestMail;
 use jeremykenedy\LaravelEmailDatabaseLog\Tests\Mail\TestMailWithAttachment;
 use jeremykenedy\LaravelEmailDatabaseLog\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\Mime\Encoder\Base64Encoder;
 
 class LaravelEmailDatabaseTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
+    #[Test]
     public function the_email_is_logged_to_the_database()
     {
         Mail::to('email@example.com')
@@ -32,7 +34,7 @@ class LaravelEmailDatabaseTest extends TestCase
         ]);
     }
 
-    /** @test */
+    #[Test]
     public function multiple_recipients_are_comma_separated()
     {
         Mail::to(['email@example.com', 'email2@example.com'])
@@ -46,7 +48,7 @@ class LaravelEmailDatabaseTest extends TestCase
         ]);
     }
 
-    /** @test */
+    #[Test]
     public function recipient_with_name_is_correctly_formatted()
     {
         Mail::to((object) ['email' => 'email@example.com', 'name' => 'John Doe'])
@@ -60,10 +62,10 @@ class LaravelEmailDatabaseTest extends TestCase
         ]);
     }
 
-    /** @test */
+    #[Test]
     public function cc_recipient_with_name_is_correctly_formatted()
     {
-        Mail::cc((object) ['email' => 'email@example.com', 'name' => 'John Do'])
+        Mail::cc((object) ['email' => 'email@example.com', 'name' => 'John Doe'])
             ->send(new TestMail());
 
         $this->assertDatabaseHas('email_log', [
@@ -74,10 +76,10 @@ class LaravelEmailDatabaseTest extends TestCase
         ]);
     }
 
-    /** @test */
+    #[Test]
     public function bcc_recipient_with_name_is_correctly_formatted()
     {
-        Mail::bcc((object) ['email' => 'email@example.com', 'name' => 'John Do'])
+        Mail::bcc((object) ['email' => 'email@example.com', 'name' => 'John Doe'])
             ->send(new TestMail());
 
         $this->assertDatabaseHas('email_log', [
@@ -88,7 +90,7 @@ class LaravelEmailDatabaseTest extends TestCase
         ]);
     }
 
-    /** @test */
+    #[Test]
     public function attachement_is_saved()
     {
         Mail::to('email@example.com')->send(new TestMailWithAttachment());
@@ -96,7 +98,7 @@ class LaravelEmailDatabaseTest extends TestCase
         $log = DB::table('email_log')->first();
 
         // TODO: Is there a beter way to tests this ?
-        $encoded = (new Base64Encoder)->encodeString(file_get_contents(__DIR__.'/../stubs/demo.txt'));
+        $encoded = (new Base64Encoder)->encodeString(file_get_contents(__DIR__ . '/../stubs/demo.txt'));
 
         $this->assertStringContainsString('Content-Type: text/plain; name=demo.txt', $log->attachments);
         $this->assertStringContainsString('Content-Transfer-Encoding: base64', $log->attachments);
